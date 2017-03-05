1dad975845629dd27ccbbfa55e6fb2b2b803036e
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-03-04 19:39:29 -0500

    finishing styles for a new website

--- js/index.js
+++ js/index.js
@@ -2,4 +2,15 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.carousel.carousel-slider').carousel({full_width: true});
+  
+  $('.carousel').carousel({
+      padding: 200    
+  });
+  autoplay()   
+  function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 4500);
+  }  
+  
 });
+
