54388c1df07ff8712c67ffff29b58cf62076215f
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-12-17 19:49:51 -0500

    Update carousel autoplay

--- js/index.js
+++ js/index.js
@@ -2,39 +2,35 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.carousel.carousel-slider').carousel({full_width: true});
-  
-  $('.carousel').carousel({
-      padding: 200    
-  });
-  autoplay()   
-  function autoplay() {
-      $('.carousel').carousel('next');
-      setTimeout(autoplay, 4500);
-  }
-  
-  $(".link1").click(function() {
-  $('html,body').animate({
-      scrollTop: $(".section1").offset().top},
-      'slow');
-  });
+    
+    $('.carousel').carousel();
+    setInterval(function() {
+    $('.carousel').carousel('next');
+    }, 5000);
+        
+    $(".link1").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section1").offset().top},
+        'slow');
+    });
 
-  $(".link2").click(function() {
-  $('html,body').animate({
-      scrollTop: $(".section2").offset().top},
-      'slow');
-  });
+    $(".link2").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section2").offset().top},
+        'slow');
+    });
 
-  $(".link3").click(function() {
-  $('html,body').animate({
-      scrollTop: $(".section3").offset().top},
-      'slow');
-  });
+    $(".link3").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".section3").offset().top},
+        'slow');
+    });
 
-  $(".link4").click(function() {
-  $('html,body').animate({
-      scrollTop: $(".page-footer").offset().top},
-      'slow');
-  });  
+    $(".link4").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".page-footer").offset().top},
+        'slow');
+    });  
   
 });
 
