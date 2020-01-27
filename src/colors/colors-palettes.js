8e2b13e8c0a5fcb22cb23e94f8a133d9cb665d86
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2020-01-27 17:32:22 +0100

    New colors

--- src/colors/colors-palettes.js
+++ src/colors/colors-palettes.js
@@ -71,5 +71,84 @@ module.exports = [
       thirdColor: "#00c2ba",
       fourthColor: "#037a90"
     },
+    //-----------
+    {
+      firstColor: "#01ffc3",
+      secondColor: "#01ffff",
+      thirdColor: "#ffb3fd",
+      fourthColor: "#9d72ff"
+    },
+    {
+      firstColor: "#a0edff",
+      secondColor: "#ebf875",
+      thirdColor: "#28cf75",
+      fourthColor: "#fe6b35"
+    },
+    {
+      firstColor: "#ffff66",
+      secondColor: "#fc6e22",
+      thirdColor: "#ff1493",
+      fourthColor: "#c24cf6"
+    },
+    {
+      firstColor: "#d9eb4b",
+      secondColor: "#00a9fe",
+      thirdColor: "#fd6bb6",
+      fourthColor: "#ef0888"
+    },
+    {
+      firstColor: "#ffe3f1",
+      secondColor: "#fe1c80",
+      thirdColor: "#ff5f01",
+      fourthColor: "#ce0000"
+    },
+    {
+      firstColor: "#fef900",
+      secondColor: "#03dddc",
+      thirdColor: "#ff822e",
+      fourthColor: "#f21a1d"
+    },
+    {
+      firstColor: "#fcf340",
+      secondColor: "#7fff00",
+      thirdColor: "#fb33db",
+      fourthColor: "#0310ea"
+    },
+    {
+      firstColor: "#e92efb",
+      secondColor: "#ff2079",
+      thirdColor: "#440bd4",
+      fourthColor: "#04005e"
+    },
+    {
+      firstColor: "#bdbdfd",
+      secondColor: "#bc75f9",
+      thirdColor: "#535eeb",
+      fourthColor: "#00218a"
+    },
+    {
+      firstColor: "#b8fb3c",
+      secondColor: "#5ce5d5",
+      thirdColor: "#7898fb",
+      fourthColor: "#001437"
+    },
+    {
+      firstColor: "#8af7e4",
+      secondColor: "#fdcbfc",
+      thirdColor: "#c6bdea",
+      fourthColor: "#48adf1"
+    },
+    {
+      firstColor: "#e1ef7e",
+      secondColor: "#ffaa01",
+      thirdColor: "#12b296",
+      fourthColor: "#027a9f"
+    },
+    {
+      firstColor: "#3cb9fc",
+      secondColor: "#b537f2",
+      thirdColor: "#8a2be2",
+      fourthColor: "#120052"
+    },
 
   ]
