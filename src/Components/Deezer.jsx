219f14e90f661f5e0f2b376e791cf35f18d2b6a5
CA <camilo.arguello.rincon@gmail.com>
2021-02-22 16:13:50 +0100

    [CA] Creating iframe with deezer

--- src/Components/Deezer.jsx
+++ src/Components/Deezer.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import styles from "../css/index.pcss";
 
-export default class Deezer extends Component{
+export default class Deezer extends Component {
+    renderIframeDeezer() {
+      return (<iframe scrolling="yes" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=420&color=EF5466&layout=&size=medium&type=charts&id=user_2173462548&title=kmiloarguello&app_id=1" width="700" height="350"></iframe>)
+    }
     renderList(){
         const { favorites } = this.props.data;
         return favorites.map((elem,i) => {
@@ -21,10 +24,12 @@ export default class Deezer extends Component{
                 <div className={styles.containerTitle}>
                     <h2>{title}</h2>
                 </div>
-                <div className={styles.containerImagesDeezer}>
+                {/*<div className={styles.containerImagesDeezer}>
                     {this.renderList()}
+                </div>*/}
+                <div className={styles.containerIframeDeezer}>
+                    {this.renderIframeDeezer()}
                 </div>
-               
             </section>
         )
     }
